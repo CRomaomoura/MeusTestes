88e8bab996c003dd4a0a891d89424f5eaff96764
CRomaomoura <201810440@aluno.fjn.edu.br>
2019-06-12 16:47:20 -0300

    SelectionSort incrementado

--- selectionSort.js
+++ selectionSort.js
@@ -1,6 +1,6 @@
 var selectionSort = function(array){
   var temp;
-  
+
   for(var i=0;i<array.length;i++){
     var mi = i;
     
@@ -13,4 +13,7 @@ var selectionSort = function(array){
    array[i] = array[mi];
    array[mi] = temp;
   }
+   return array;
 };
+ var array = [8,9,5,7,3,1];
+ console.log('selectionSort should [1,3,5,7,8,9]-->', selectionSort(array));
