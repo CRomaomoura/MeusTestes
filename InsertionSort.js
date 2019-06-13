40d55f9d10b9e691beaff3942f4c4a261d2a2460
CRomaomoura <201810440@aluno.fjn.edu.br>
2019-06-13 08:29:52 -0300

    Inserindo o InsertionSort

---
+++ InsertionSort.js
@@ -0,0 +1,14 @@
+const insertionSort = array => {
+  const len = array.length;//guarda o comprimento do array
+  for (let i = 0; i < len; i++) {
+    let el = array[i];//manter o valor atual do laço
+    let j;//var p/manter posicao atual
+    for (j = i - 1; j >= 0 && array[j] > el; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = el; // inserir no array na posiçao atual +1 
+  }
+  return array; // retornando o array
+};
+const test = insertionSort([2,4,6,9,5])
+document.write(test);
